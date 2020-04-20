c5cb6164982804f4aacfc8d267ca69ddbec4f2ee
justjazz903 <31698772+justjazz903@users.noreply.github.com>
2020-04-20 15:19:04 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://35.194.31.251:8080';
+export const API_ROOT = 'https://';
 export const TOKEN_KEY = 'TOKEN';
 export const GEO_OPTIONS = {
   enableHighAccuracy: true,
@@ -10,4 +10,4 @@ export const AUTH_HEADER = 'Bearer';
 export const POS_NOISE = 0.02;
 export const POST_TYPE_IMAGE = 'image';
 export const POST_TYPE_VIDEO = 'video';
-export const MAP_API_KEY = 'AIzaSyDSpILn2ZPvZ4r4pUJhfw2L4uicFayktEE';
+export const MAP_API_KEY = '';
